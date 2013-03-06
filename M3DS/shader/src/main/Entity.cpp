21a1b23a605f5498d321751f28dfd3b61479bbcf
ruytoor <benjamin.ruytoor@gmail.com>
2013-03-06 12:14:38 +0100

    modified:   M3DS/shader-build-desktop-Qt_4_8_1_dans_le_PATH__Syst_me__Release/Entity.o 	modified:   M3DS/shader/bin/shader 	modified:   M3DS/shader/shader.pro.user 	modified:   M3DS/shader/src/main/Entity.cpp

--- M3DS/shader/src/main/Entity.cpp
+++ M3DS/shader/src/main/Entity.cpp
@@ -14,41 +14,55 @@ using namespace std;
 
 // Mapping de la structure de données stockée dans obj vers buffers OpenGL
 void Entity::initBuffer() {
-  // On commence par tout mettre dans des tableaux "bas niveaux" pour pouvoir les passer à OpenGL
-  float *pts;
-  pts=new float[this->nbVertex()*3]; // *3 car on mettra pour chaque sommet les 3 coordonnées x,y,z
-  unsigned int *indice;
-  indice=new unsigned int[this->nbFace()*3]; // *3 car on doit mettre les 3 indices constituant chacun des triangles.
+    // On commence par tout mettre dans des tableaux "bas niveaux" pour pouvoir les passer à OpenGL
+    float *pts;
+    pts=new float[this->nbVertex()*3]; // *3 car on mettra pour chaque sommet les 3 coordonnées x,y,z
+    unsigned int *indice;
+    indice=new unsigned int[this->nbFace()*3]; // *3 car on doit mettre les 3 indices constituant chacun des triangles.
 
 
 
 
-  // A Compléter : mapping dans les tableaux + initialisation des buffers OpenGL
-  // this->nbVertex() donne le nombre de sommets de l'objet
-  // this->nbFace() donne le nombre de l'objet (CU : l'objet doit être uniquement constitué de triangles)
-  // this->vertex(i) donne les coordonnées du i-ieme sommet (de type Vector3 : this->vertex(i).x() pour x, this->vertex(i).y() pour y, this->vertex(i).z() pour z).
-  // this->normalVertex(i) donne les coordonnées de la normale du i-ieme sommet (de type Vector3).
-  // this->indexVertex(i,j) donne l'indice k du j-ieme sommet dans la face i (k étant l'indice dans le tableau des sommets : this->vertex(k) pour avoir ses coordonnées).
+    // A Compléter : mapping dans les tableaux + initialisation des buffers OpenGL
+    // this->nbVertex() donne le nombre de sommets de l'objet
+    // this->nbFace() donne le nombre de l'objet (CU : l'objet doit être uniquement constitué de triangles)
+    // this->vertex(i) donne les coordonnées du i-ieme sommet (de type Vector3 : this->vertex(i).x() pour x, this->vertex(i).y() pour y, this->vertex(i).z() pour z).
+    // this->normalVertex(i) donne les coordonnées de la normale du i-ieme sommet (de type Vector3).
+    // this->indexVertex(i,j) donne l'indice k du j-ieme sommet dans la face i (k étant l'indice dans le tableau des sommets : this->vertex(k) pour avoir ses coordonnées).
 
-  // 1) initialiser les tableaux pts et indice
+    // 1) initialiser les tableaux pts et indice
 
-  // 2) créer les buffers openGL et recopier les tableaux pts et indice dans ces buffers
-  glGenBuffers(1,&_vertexBuffer);
-  glGenBuffers(1,&_indiceBuffer);
+    for(int i=0;i<this->nbVertex()*3;i+=3){
+        pts[i]=this->vertex(i).x();
+        pts[i+1]=this->vertex(i).y();
+        pts[i+2]=this->vertex(i).z();
+    }
+
+    for(int i=0;i<this->nbFace()*3;i+=3){
+        indice[i]=this->indexVertex(i,0);
+        indice[i+1]=this->indexVertex(i,1);
+        indice[i+2]=this->indexVertex(i,2);
+    }
+    // 2) créer les buffers openGL et recopier les tableaux pts et indice dans ces buffers
+    glGenBuffers(1,&_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER,_vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER,this->nbVertex()*3*sizeof(GLfloat),pts,GL_STATIC_DRAW);
 
 
+    glGenBuffers(1,&_indiceBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,_indiceBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,this->nbFace()*3*sizeof(GLfloat),indice,GL_STATIC_DRAW);
 
-
-
-  // on supprime les tableaux de la mémoire centrale (tout est maintenant dans la mémoire OpenGL)
-  delete[] pts;
-  delete[] indice;
+    // on supprime les tableaux de la mémoire centrale (tout est maintenant dans la mémoire OpenGL)
+    delete[] pts;
+    delete[] indice;
 
 }
 
 
 void Entity::drawBuffer() {
-  // TODO
+    // TODO
+
 
 }
 
@@ -62,10 +76,10 @@ Entity::~Entity() { //dtor
 }
 
 void Entity::read(string nom) {
-  MeshObj::read(nom);
-  this->triangulate(); // assure un modèle triangulaire (suppose des faces convexes)
-  this->computeNormal();
-  this->scaleInBoxMin(-2,2,-2,2,-2,2); // le modèle sera dans la boite x=[-2,2],y=[-2,2] etc (changement d'échelle)
+    MeshObj::read(nom);
+    this->triangulate(); // assure un modèle triangulaire (suppose des faces convexes)
+    this->computeNormal();
+    this->scaleInBoxMin(-2,2,-2,2,-2,2); // le modèle sera dans la boite x=[-2,2],y=[-2,2] etc (changement d'échelle)
 }
 
 
