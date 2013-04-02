8d19e3e9bf877c0472d3f9cbee2eba102026529d
ruytoor <benjamin.ruytoor@gmail.com>
2013-04-02 13:57:51 +0200

    ajout fichier IHM p2

--- TI/TP9/TESTPlugin_/src/TESTPlugin_.java
+++ TI/TP9/TESTPlugin_/src/TESTPlugin_.java
@@ -1,22 +1,219 @@
-// Importation des paquets nécessaires. Le plugin n'est pas lui-même un paquet (pas de mot-clé package)
-import ij.*; // pour classes ImagePlus et IJ
-import ij.plugin.filter.PlugInFilter; // pour interface PlugInFilter
-import ij.process.*; // pour classe ImageProcessor
-import java.awt.*; // pour classe Rectangle
+/**
+ * Laplacien_.java 
+ *
+ * Dï¿½tection de points contours par approches du second ordre.
+ * Fichier ï¿½tudiant ï¿½ complï¿½ter
+ *
+ * @author 
+ *
+ */
 
-// Nom de la classe = nom du fichier. Implémente l'interface PlugInFilter
-public class TESTPlugin_ implements PlugInFilter {
+import ij.*;	// pour classes ImagePlus et IJ
+import ij.gui.*;	// pour classes GenericDialog et DialogListener
+import ij.plugin.filter.*;	// pour interface PlugInFilter et Convolver
+import ij.process.*;	// pour classe ImageProcessor et sous-classes
+import java.awt.*;		//pour classes AWTEvent, CheckBox, TextField
+import java.util.Vector;	// pour classe Vector
+
+
+public class TESTPlugin_ implements PlugInFilter, DialogListener {
+
+	private static int filtre=0;
+	private final static String[] FILTRES_LAPLACIENS3x3 = {"Laplacien1", "Laplacien2", "Laplacien3","Laplacien4","Laplacien5"};
+	private final static float[][] MASQUES_LAPLACIENS3x3 = {
+		{0,1,0, 1,-4,1, 0,1,0},
+		{1,0,1, 0,-4,0, 1,0,1},
+		{1,1,1, 1,-8,1, 1,1,1},
+		{1,2,1, 2,-12,2, 1,2,1},
+		{1,4,1, 4,-20,4, 1,4,1}
+	};
+	private static float sigma=0.0f;
+	private static boolean seuillageZeroCross=false;
+	private static boolean seuilZeroCrossAuto=true;
+	private static float seuilZeroCross=10f;
+
+	private ImagePlus imp;
+
+	// ---------------------------------------------------------------------------------
+	// Mï¿½thodes de l'interface PlugInFilter
+
+	// Initialisation du plugin
 	public int setup(String arg, ImagePlus imp) {
-		if (IJ.versionLessThan("1.37j")) // Requiert la version 1.37j d'ImageJ
-			return DONE; // Ne pas appeler la méthode run()
-		else // Accepte tous types d'images, piles d'images et RoIs, même non rectangulaires
-			return DOES_8G+DOES_RGB+DOES_STACKS+SUPPORTS_MASKING;
+		this.imp = imp;
+		return PlugInFilter.DOES_8G;
 	}
-	
+
+	// Mï¿½thode principale du plugin
 	public void run(ImageProcessor ip) {
-		Rectangle r = ip.getRoi(); // Région d'intérêt sélectionnée (r.x=r.y=0 si aucune)
-		for (int y=r.y; y<(r.y+r.height); y++)
-			for (int x=r.x; x<(r.x+r.width); x++)
-				ip.set(x, y, ~ip.get(x,y)); // Complément bit à bit des valeurs des pixels
+
+		// Affichage de la fenï¿½tre de configuration
+		if (!showDialog())
+			return;
+
+		// Titre et extension de l'image source
+		String titre = imp.getTitle();
+		String extension="";
+		int index = titre.lastIndexOf('.');
+		if (index>0)
+			extension = titre.substring(index);
+		else
+			index = titre.length();
+		titre = titre.substring(0,index);		
+
+		// Gï¿½nï¿½ration d'un masque LoG de taille impaire (exercice 3)
+		/* ï¿½ complï¿½ter */
+
+
+		// Calcul et reprï¿½sentation du Laplacien (exercice 1, puis ï¿½ modifier dans le 3) 
+		FloatProcessor fpLaplacian = (FloatProcessor)(ip.duplicate().convertToFloat());
+		/* ï¿½ complï¿½ter */
+		fpLaplacian.convolve(MASQUES_LAPLACIENS3x3[filtre], 3, 3);
+		ImagePlus im=new ImagePlus("f1", fpLaplacian);
+		im.show();
+		ImagePlus im2=new ImagePlus("f2",laplacienZero(fpLaplacian, seuilZeroCross));
+		im2.show();
+		// Dï¿½tection et affichage des passages par 0 du Laplacien par seuillage du Laplacien (exercice 2)
+		if (seuillageZeroCross) {
+			/* ï¿½ complï¿½ter */
+		}
 	}
+
+	// ---------------------------------------------------------------------------------
+	/**
+	 * Mï¿½thode d'affichage de la fenï¿½tre de config et de lecture des valeurs saisies, appelï¿½e dans run()
+	 * 
+	 * @return false si la fenï¿½tre a ï¿½tï¿½ fermï¿½e en cliquant sur Cancel, true sinon (boolean)
+	 */ 
+
+	public boolean showDialog() {
+
+		// Description de la fenï¿½tre de config
+		GenericDialog gd = new GenericDialog("Laplacian parameters");
+		gd.addChoice("Laplacian filter type:", FILTRES_LAPLACIENS3x3, FILTRES_LAPLACIENS3x3[filtre]);
+		gd.addNumericField("Gaussian filtering scale (0 for none)", sigma, 1);
+		gd.addCheckbox("Threshold Laplacian zero-crossings", seuillageZeroCross);
+		gd.addNumericField("Threshold value", seuilZeroCross, 0);
+		gd.getComponent(gd.getComponentCount()-1).setEnabled(seuillageZeroCross && !seuilZeroCrossAuto); // Dï¿½sactiver champ ?
+		gd.addCheckbox ("Auto threshold", seuilZeroCrossAuto);
+		gd.getComponent(gd.getComponentCount()-1).setEnabled(seuillageZeroCross && seuilZeroCrossAuto); // Dï¿½sactiver case ?
+		gd.addDialogListener(this);     		// the DialogItemChanged method will be called on user input
+		gd.showDialog();                		// display the dialog; preview runs in the background now
+		if (gd.wasCanceled()) return false;
+
+		// Lecture des valeurs saisies
+		filtre = gd.getNextChoiceIndex();
+		sigma = (float) gd.getNextNumber();
+		seuillageZeroCross = gd.getNextBoolean();
+		seuilZeroCross = (float) gd.getNextNumber();
+		seuilZeroCrossAuto = gd.getNextBoolean();
+
+		return true;
+	}	
+
+	// ---------------------------------------------------------------------------------
+	/**
+	 * Mï¿½thode appelï¿½e sur modification (par un ï¿½vï¿½nement) de la fenï¿½tre de config
+	 * 
+	 * @param gd Fenï¿½tre de dialogue (GenericDialog)
+	 * @param e ï¿½vï¿½nement ï¿½ traiter (AWTEvent) 
+	 * @return true si la saisie est correcte, false sinon (boolean)
+	 */
+
+	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
+
+		// Accï¿½s aux champs de la fenï¿½tre
+		Checkbox zcCheckbox = (Checkbox)gd.getCheckboxes().get(0);
+		Checkbox zcAutoCheckbox = (Checkbox)gd.getCheckboxes().get(1);
+		Vector numFields = gd.getNumericFields();
+		TextField sigmaField = (TextField)numFields.get(1);
+		TextField zcThresholdField = (TextField)numFields.get(1);
+		if (e!=null) {	// e==null si clic sur OK
+			// Dï¿½sactivation/activation du champ numï¿½rique de seuil, selon seuillage ou pas
+			if (e.getSource() == zcCheckbox) { 
+				zcThresholdField.setEnabled(zcCheckbox.getState()&&!zcAutoCheckbox.getState());
+				zcAutoCheckbox.setEnabled(zcCheckbox.getState());
+			}
+			else if (e.getSource() == zcAutoCheckbox) {
+				zcThresholdField.setEnabled(!zcAutoCheckbox.getState());
+			}
+		}
+		sigma = Float.valueOf(sigmaField.getText());
+		seuilZeroCross = Integer.valueOf(zcThresholdField.getText());
+		return (!gd.invalidNumber() && sigma>=0 && seuilZeroCross>=0);
+	}
+
+	// ---------------------------------------------------------------------------------
+
+	/**
+	 * Dï¿½tection des passages par 0 du Laplacien (algo min<-seuil && max>seuil)
+	 * 
+	 * @param imLaplacien Image du Laplacien (ImageProcessor 32 bits)
+	 * @param seuil Seuil sur les valeurs du Laplacien
+	 * @return imZeros Carte des passages par 0 (image binaire)
+	 */
+	public ByteProcessor laplacienZero(ImageProcessor imLaplacien, Float seuil) {
+
+		int width = imLaplacien.getWidth();
+		int height = imLaplacien.getHeight();
+		
+		// Image binaire rï¿½sultat des points contours aprï¿½s seuillage
+		ByteProcessor imZeros = new ByteProcessor(width,height);
+		for(int x=1;x<width;++x){
+			for(int y=1;y<height;++y){
+				float min=Float.MAX_VALUE;
+				float max=Float.MIN_VALUE;
+				for(int i=-1;i<2;++i){
+					for(int j=-1;j<2;++j){
+						if(imLaplacien.getPixel(x+i, y+j)>max)
+							max=imLaplacien.getPixelValue(x+i, y+j);
+						if(imLaplacien.getPixel(x+i, y+j)<min)
+							min=imLaplacien.getPixelValue(x+i, y+j);
+					}
+				}
+				if((max-min)>seuil)
+					imZeros.set(x, y, 255);
+				else
+					imZeros.set(x, y, 0);
+			}
+
+		}
+		return imZeros;
+	}
+
+	// ---------------------------------------------------------------------------------
+
+	/**
+	 * Gï¿½nï¿½ration d'un masque LoG dans un tableau float[]
+	 * 
+	 * @param tailleMasque nombre de lignes et de colonnes du masque LoG (int, impair).
+	 * @param sigma ï¿½cart-type de la gaussienne (float, au moins 5*sigma)
+	 * @return masque de l'opï¿½rateur LoG
+	 */
+	public static float[] masqueLoG(int tailleMasque, float sigma)
+	{     
+		short aperture = (short)(tailleMasque/2);
+		double[][] LoG = new double[2*aperture+1][2*aperture+1];
+		float[] out=new float[(2*aperture+1)*(2*aperture+1)];
+		double sum=0, s2=Math.pow(sigma,2);
+		int k=0;
+
+		// Calcul du masque LoG
+		for(int dy=-aperture;dy<=aperture;dy++)
+		{
+			for(int dx=-aperture;dx<=aperture;dx++)
+			{
+				double r2=-(dx*dx+dy*dy)/2.0/s2;
+				LoG[dy+aperture][dx+aperture]=-1/(Math.PI*s2*s2)*(1+r2)*Math.exp(r2);
+				sum+=LoG[dy+aperture][dx+aperture];
+			}
+		}
+		// Soustraction de la moyenne pour obtenir une somme nulle des coefs
+		sum=sum/(tailleMasque*tailleMasque);
+		for(int dy=-aperture;dy<=aperture;dy++)
+			for(int dx=-aperture;dx<=aperture;dx++)
+				out[k++]= (float)(LoG[dy+aperture][dx+aperture]-sum);
+
+		return out;
+	}
+
 }
