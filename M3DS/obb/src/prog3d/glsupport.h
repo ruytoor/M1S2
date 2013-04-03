fc2ff5d9268b13e76b34b2d4c04c135918bf7dc8
allart <aurore.allart@gmail.com>
2013-04-03 10:53:46 +0200

    M3DS obb ajout fichier

--- M3DS/obb/src/prog3d/glsupport.h
+++ M3DS/obb/src/prog3d/glsupport.h
@@ -1,11 +1,8 @@
 #ifndef GLSUPPORT_H
 #define GLSUPPORT_H
 
-#define GL_GLEXT_PROTOTYPES
+#include <GL/glew.h>
 #include <QGLWidget>
-#include <GL/gl.h>
-#include <GL/glext.h>
-#include <GL/glu.h>
 
 #endif // GLSUPPORT_H
 
