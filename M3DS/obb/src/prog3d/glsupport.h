d01c9e2b84bf22a43ed5659552a8bcc21a281bbf
allart <aurore.allart@gmail.com>
2013-04-03 10:26:14 +0200

    M3DS obb

---
+++ M3DS/obb/src/prog3d/glsupport.h
@@ -0,0 +1,11 @@
+#ifndef GLSUPPORT_H
+#define GLSUPPORT_H
+
+#define GL_GLEXT_PROTOTYPES
+#include <QGLWidget>
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glu.h>
+
+#endif // GLSUPPORT_H
+
--- workspace/Projet2IHM/src/vue/bibliotheques.java
+++
@@ -1,7 +0,0 @@
-package vue;
-
-import javax.swing.JPanel;
-
-public class bibliotheques extends JPanel {
-
-}
