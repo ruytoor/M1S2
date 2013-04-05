4901544beb59b3bc56ddf2a5d570ed60613bf213
ruytoor <benjamin.ruytoor@gmail.com>
2013-04-05 15:56:07 +0200

    TI

--- TI/workspace/TESTPlugin_/src/TESTPlugin_.java
+++ TI/workspace/TESTPlugin_/src/TESTPlugin_.java
@@ -1,263 +1,87 @@
-/**
- * Laplacien_.java 
- *
- * D�tection de points contours par approches du second ordre.
- * Fichier �tudiant � compl�ter
- *
- * @author 
- *
- */
+import ij.*;
+import ij.plugin.filter.*;
+import ij.process.*;
+import ij.gui.*;
 
-import ij.*;	// pour classes ImagePlus et IJ
-import ij.gui.*;	// pour classes GenericDialog et DialogListener
-import ij.plugin.filter.*;	// pour interface PlugInFilter et Convolver
-import ij.process.*;	// pour classe ImageProcessor et sous-classes
-import java.awt.*;		//pour classes AWTEvent, CheckBox, TextField
-import java.util.Vector;	// pour classe Vector
+public class TESTPlugin_ implements PlugInFilter {
+
+	ImagePlus imp;	// Fenêtre contenant l'image de référence
+	int width;		// Largeur de la fenêtre
+	int height;		// Hauteur de la fenêtre
 
 
-public class TESTPlugin_ implements PlugInFilter, DialogListener {
-
-	private static int filtre=0;
-	private final static String[] FILTRES_LAPLACIENS3x3 = {"Laplacien1", "Laplacien2", "Laplacien3","Laplacien4","Laplacien5"};
-	private final static float[][] MASQUES_LAPLACIENS3x3 = {
-		{0,1,0, 1,-4,1, 0,1,0},
-		{1,0,1, 0,-4,0, 1,0,1},
-		{1,1,1, 1,-8,1, 1,1,1},
-		{1,2,1, 2,-12,2, 1,2,1},
-		{1,4,1, 4,-20,4, 1,4,1}
-	};
-
-	private static float sigma=0.0f;
-	private static boolean seuillageZeroCross=false;
-	private static boolean seuilZeroCrossAuto=true;
-	private static float seuilZeroCross=12f;
-
-	private ImagePlus imp;
-	// ---------------------------------------------------------------------------------
-	// M�thodes de l'interface PlugInFilter
-
-	// Initialisation du plugin
 	public int setup(String arg, ImagePlus imp) {
+		/* à compléter*/
 		this.imp = imp;
-		return PlugInFilter.DOES_8G;
+		return PlugInFilter.DOES_RGB;
 	}
 
-	// M�thode principale du plugin
+
 	public void run(ImageProcessor ip) {
+		// Lecture des dimensions de la fenêtre
+		width = imp.getWidth();
+		height = imp.getHeight();
 
-		// Affichage de la fen�tre de configuration
-		if (!showDialog())
-			return;
+		// Dispositions possibles pour le CFA
+		String[] orders = {"R-G-R", "B-G-B", "G-R-G", "G-B-G-B"};
 
-		// Titre et extension de l'image source
-		String titre = imp.getTitle();
-		String extension="";
-		int index = titre.lastIndexOf('.');
-		if (index>0)
-			extension = titre.substring(index);
-		else
-			index = titre.length();
-		titre = titre.substring(0,index);		
+		// Définition de l'interface
+		GenericDialog dia = new GenericDialog("Génération de l'image CFA...", IJ.getInstance());
+		dia.addChoice("Début de première ligne :", orders, orders[2]);
+		dia.showDialog();
 
-		// G�n�ration d'un masque LoG de taille impaire (exercice 3)
-		/* � compl�ter */
+		// Lecture de la réponse de l'utilisateur
+		if (dia.wasCanceled()) return;
+		int order = dia.getNextChoiceIndex();
 
-
-		// Calcul et repr�sentation du Laplacien (exercice 1, puis � modifier dans le 3) 
-		FloatProcessor fpLaplacian = (FloatProcessor)(ip.duplicate().convertToFloat());
-		FloatProcessor fpLaplacian2 = (FloatProcessor)(ip.duplicate().convertToFloat());
-		/* � compl�ter */
-		fpLaplacian.convolve(MASQUES_LAPLACIENS3x3[filtre], 3, 3);
-		ImagePlus im=new ImagePlus("f1", fpLaplacian);
-		im.show();
-		ImagePlus im2=new ImagePlus("f2",laplacienZero(fpLaplacian, seuilZeroCross));
-		im2.show();
-		//int taille=Math.round(7*sigma);
-		int taille=(int)(7*sigma);
-		if(taille%2==0)
-			taille++;
-		System.out.println(taille);
-		float []mas=masqueLoG(taille,sigma);
-		Convolver conv =new Convolver();
-		conv.setNormalize(false);
-		if(!conv.convolve(fpLaplacian2, mas, taille,taille)){
-			System.out.println("Erreur");
-			return;
-		}
-		ImagePlus im3=new ImagePlus("f3",fpLaplacian2);
-		im3.show();
-		ImagePlus im4=new ImagePlus("f4",laplacienZero(fpLaplacian2, 0.1f));
-		im4.show();
-
-		// D�tection et affichage des passages par 0 du Laplacien par seuillage du Laplacien (exercice 2)
-		if (seuillageZeroCross) {
-			/* � compl�ter */
-		}
+		// Génération de l'image CFA
+		/* à compléter */
+		ImagePlus impCFA=new ImagePlus("CFA", cfa(order));
+		impCFA.show();
 	}
 
-	// ---------------------------------------------------------------------------------
-	/**
-	 * M�thode d'affichage de la fen�tre de config et de lecture des valeurs saisies, appel�e dans run()
-	 * 
-	 * @return false si la fen�tre a �t� ferm�e en cliquant sur Cancel, true sinon (boolean)
-	 */ 
+	//Génère l'image CFA
+	ImageProcessor cfa(int row_order) {
+		// Image couleur de référence et ses dimensions
+		ImageProcessor ip = imp.getProcessor();
+		width = imp.getWidth();
+		height = imp.getHeight();
 
-	public boolean showDialog() {
+		int pixel_value = 0;	// Valeur du pixel source
+		ImageProcessor cfa_ip = new ByteProcessor(width,height);	// Image CFA générée
 
-		// Description de la fen�tre de config
-		GenericDialog gd = new GenericDialog("Laplacian parameters");
-		gd.addChoice("Laplacian filter type:", FILTRES_LAPLACIENS3x3, FILTRES_LAPLACIENS3x3[filtre]);
-		gd.addNumericField("Gaussian filtering scale (0 for none)", sigma, 1);
-		gd.addCheckbox("Threshold Laplacian zero-crossings", seuillageZeroCross);
-		gd.addNumericField("Threshold value", seuilZeroCross, 0);
-		gd.getComponent(gd.getComponentCount()-1).setEnabled(seuillageZeroCross && !seuilZeroCrossAuto); // D�sactiver champ ?
-		gd.addCheckbox ("Auto threshold", seuilZeroCrossAuto);
-		gd.getComponent(gd.getComponentCount()-1).setEnabled(seuillageZeroCross && seuilZeroCrossAuto); // D�sactiver case ?
-		gd.addDialogListener(this);     		// the DialogItemChanged method will be called on user input
-		gd.showDialog();                		// display the dialog; preview runs in the background now
-		if (gd.wasCanceled()) return false;
+		// Échantillons G
+		for (int y=0; y<height; y+=2) {
+			for (int x=0; x<width; x+=2) {
+				pixel_value = ip.getPixel(x,y);
+				int green = (int)(pixel_value & 0x00ff00)>>8;
+			cfa_ip.putPixel(x,y,green);
+			}
+		}
+		for (int y=1; y<height; y+=2) {
+			for (int x=1; x<width; x+=2) {
+				pixel_value = ip.getPixel(x,y);
+				int green = (int)(pixel_value & 0x00ff00)>>8;
+			cfa_ip.putPixel(x,y,green);
+			}
+		}
+		// Échantillons R
+		for (int y=0; y<height; y+=2) {
+			for (int x=1; x<width; x+=2) {
+				pixel_value = ip.getPixel(x,y);
+				int red = (int)(pixel_value & 0xff0000)>>16;
+			cfa_ip.putPixel(x,y,red);
+			}
+		}
+		// Échantillons B
+		for (int y=1; y<height; y+=2) {
+			for (int x=0; x<width; x+=2) {
+				pixel_value = ip.getPixel(x,y);
+				int blue = (int)(pixel_value & 0x0000ff);
+				cfa_ip.putPixel(x,y,blue);
+			}
+		}
 
-		// Lecture des valeurs saisies
-		filtre = gd.getNextChoiceIndex();
-		sigma = (float) gd.getNextNumber();
-		seuillageZeroCross = gd.getNextBoolean();
-		seuilZeroCross = (float) gd.getNextNumber();
-		seuilZeroCrossAuto = gd.getNextBoolean();
-
-		return true;
+		return cfa_ip;
 	}	
-
-	// ---------------------------------------------------------------------------------
-	/**
-	 * M�thode appel�e sur modification (par un �v�nement) de la fen�tre de config
-	 * 
-	 * @param gd Fen�tre de dialogue (GenericDialog)
-	 * @param e �v�nement � traiter (AWTEvent) 
-	 * @return true si la saisie est correcte, false sinon (boolean)
-	 */
-
-	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
-
-		// Acc�s aux champs de la fen�tre
-		Checkbox zcCheckbox = (Checkbox)gd.getCheckboxes().get(0);
-		Checkbox zcAutoCheckbox = (Checkbox)gd.getCheckboxes().get(1);
-		Vector numFields = gd.getNumericFields();
-		TextField sigmaField = (TextField)numFields.get(1);
-		TextField zcThresholdField = (TextField)numFields.get(1);
-		if (e!=null) {	// e==null si clic sur OK
-			// D�sactivation/activation du champ num�rique de seuil, selon seuillage ou pas
-			if (e.getSource() == zcCheckbox) { 
-				zcThresholdField.setEnabled(zcCheckbox.getState()&&!zcAutoCheckbox.getState());
-				zcAutoCheckbox.setEnabled(zcCheckbox.getState());
-			}
-			else if (e.getSource() == zcAutoCheckbox) {
-				zcThresholdField.setEnabled(!zcAutoCheckbox.getState());
-			}
-		}
-		sigma = Float.valueOf(sigmaField.getText());
-		seuilZeroCross = Integer.valueOf(zcThresholdField.getText());
-		return (!gd.invalidNumber() && sigma>=0 && seuilZeroCross>=0);
-	}
-
-	// ---------------------------------------------------------------------------------
-
-	/**
-	 * D�tection des passages par 0 du Laplacien (algo min<-seuil && max>seuil)
-	 * 
-	 * @param imLaplacien Image du Laplacien (ImageProcessor 32 bits)
-	 * @param seuil Seuil sur les valeurs du Laplacien
-	 * @return imZeros Carte des passages par 0 (image binaire)
-	 */
-	public ByteProcessor laplacienZero(ImageProcessor imLaplacien, Float seuil) {
-
-		int width = imLaplacien.getWidth();
-		int height = imLaplacien.getHeight();
-
-		// Image binaire r�sultat des points contours apr�s seuillage
-		ByteProcessor imZeros = new ByteProcessor(width,height);
-//		for(int x=1;x<width;++x){
-//			for(int y=1;y<height;++y){
-//				float min=Float.MAX_VALUE;
-//				float max=Float.MIN_VALUE;
-//				for(int i=-1;i<2;++i){
-//					for(int j=-1;j<2;++j){
-//						if(imLaplacien.getPixel(x+i, y+j)>max)
-//							max=imLaplacien.getPixelValue(x+i, y+j);
-//						if(imLaplacien.getPixel(x+i, y+j)<min)
-//							min=imLaplacien.getPixelValue(x+i, y+j);
-//					}
-//				}
-//				if((max-min)>seuil)
-//					imZeros.set(x, y, 255);
-//				else
-//					imZeros.set(x, y, 0);
-//			}
-//
-//		}
-//		return imZeros;
-		// Pour chaque pixel (x,y),
-		for(int x=1;x<width;x++){
-			for(int y = 1;y< height;y++){
-				float pixelC = imLaplacien.getPixelValue(x,y);		// Pixel central
-				float pixelD = imLaplacien.getPixelValue(x+1,y);	// Pixel droit
-				float pixelB = imLaplacien.getPixelValue(x,y+1);	// Pixel bas
-				float pixelBD = imLaplacien.getPixelValue(x+1,y+1);	// Pixel bas droit
-				// D�tection des transitions Horizontales
-				if (pixelC<-seuil && pixelD>seuil) { // Transition horizontale -|+
-					imZeros.set(x,y,255); }
-				if (pixelC>seuil && pixelD<-seuil) { // Transition horizontale +|-
-					imZeros.set(x+1,y,255);}
-				// D�tection des transitions Verticales
-				if (pixelC<-seuil && pixelB>seuil) { // Transition verticale -|+
-					imZeros.set(x,y,255);}
-				if (pixelC>seuil && pixelB<-seuil) { // Transition verticale +|-
-					imZeros.set(x,y+1,255);}
-				// D�tection des transitions Diagonales
-				if (pixelC<-seuil && pixelBD>seuil) { // Transition diagonale -|+
-					imZeros.set(x,y,255);}
-				if (pixelC>seuil && pixelBD<-seuil) { // Transition diagonale +|-
-					imZeros.set(x+1,y+1,255);
-				}				
-			}
-		}
-		return imZeros;
-
-	}
-
-	// ---------------------------------------------------------------------------------
-
-	/**
-	 * G�n�ration d'un masque LoG dans un tableau float[]
-	 * 
-	 * @param tailleMasque nombre de lignes et de colonnes du masque LoG (int, impair).
-	 * @param sigma �cart-type de la gaussienne (float, au moins 5*sigma)
-	 * @return masque de l'op�rateur LoG
-	 */
-	public static float[] masqueLoG(int tailleMasque, float sigma)
-	{     
-		short aperture = (short)(tailleMasque/2);
-		double[][] LoG = new double[2*aperture+1][2*aperture+1];
-		float[] out=new float[(2*aperture+1)*(2*aperture+1)];
-		double sum=0, s2=Math.pow(sigma,2);
-		int k=0;
-
-		// Calcul du masque LoG
-		for(int dy=-aperture;dy<=aperture;dy++)
-		{
-			for(int dx=-aperture;dx<=aperture;dx++)
-			{
-				double r2=-(dx*dx+dy*dy)/2.0/s2;
-				LoG[dy+aperture][dx+aperture]=-1/(Math.PI*s2*s2)*(1+r2)*Math.exp(r2);
-				sum+=LoG[dy+aperture][dx+aperture];
-			}
-		}
-		// Soustraction de la moyenne pour obtenir une somme nulle des coefs
-		sum=sum/(tailleMasque*tailleMasque);
-		for(int dy=-aperture;dy<=aperture;dy++)
-			for(int dx=-aperture;dx<=aperture;dx++)
-				out[k++]= (float)(LoG[dy+aperture][dx+aperture]-sum);
-
-		return out;
-	}
 }
